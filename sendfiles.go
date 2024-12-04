f7dd6ba531f4365653a6b8dc3c9b827c73398e55
David Capello <davidcapello@gmail.com>
2024-12-03 23:58:30 -0300

    Improve order to scan IPs
    We should first scan for 192.168.0.0 to 192.168.255.255 IPs, which is
    the shortest range to scan. Then 172.16.0.0 to 172.31.255.255. And
    finally 10.0.0.0 to 10.255.255.255.

--- sendfiles.go
+++ sendfiles.go
@@ -18,6 +18,8 @@ import (
 	"time"
 )
 
+var done = false
+
 func sendFile(conn net.Conn, fn string) {
 	f, err := os.Open(fn)
 	if err != nil {
@@ -32,9 +34,7 @@ func sendFile(conn net.Conn, fn string) {
 
 func connectToServer(ip net.IP, files []string) {
 	conn, err := net.Dial("tcp", ip.String() + ":" + strconv.Itoa(port))
-	if err != nil {
-		return
-	}
+	if err != nil { return }
 
 	if conn != nil {
 		fmt.Println("Connected to " + ip.String())
@@ -72,29 +72,72 @@ func connectToServer(ip net.IP, files []string) {
 
 			sendFile(conn, fn)
 			fmt.Printf(" OK\n")
+			done = true
 		}
 
 		fmt.Fprintf(conn, "done\n")
-		time.Sleep(5 * time.Second)
+		time.Sleep(time.Second)
 		conn.Close()
 	}
 }
 
 func scanIps(files []string) {
 	ips := getIpAddresses(make([]net.IP, 0))
+
+	// Check for private network IP4 addresses
+	fmt.Println("Scanning 192.168.0.0 to 192.168.255.255 range...")
 	for _, ip := range ips {
 		ip4 := ip.To4()
-		// Check if the IP4 address is from a private network
-		if ip4 != nil &&
-			((ip4[0] == 10) ||
-			 (ip4[0] == 172 && (ip4[1] >= 16 && ip4[1] <= 31)) ||
-			 (ip4[0] == 192 && ip4[1] == 168)) {
+		if ip4 != nil && ip4[0] == 192 && ip4[1] == 168 {
 			for i := 0; i <= 255; i++ {
-				serverIp4 := net.IPv4(ip4[0], ip4[1], ip4[2], byte(i))
-				go connectToServer(serverIp4, files)
+				for j := 0; j <= 255; j++ {
+					if byte(i) != ip4[2] && byte(j) != ip4[3] {
+						serverIp4 := net.IPv4(ip4[0], ip4[1], byte(i), byte(j))
+						connectToServer(serverIp4, files)
+						if done { return }
+					}
+				}
 			}
 		}
 	}
+
+	fmt.Println("Scanning 172.16.0.0 to 172.31.255.255 range...")
+	for _, ip := range ips {
+		ip4 := ip.To4()
+		if ip4 != nil && ip4[0] == 172 && (ip4[1] >= 16 && ip4[1] <= 31) {
+			for i := 16; i <= 31; i++ {
+				for j := 0; j <= 255; j++ {
+					for k := 0; k <= 255; k++ {
+						if byte(i) != ip4[1] && byte(j) != ip4[2] && byte(k) != ip4[3] {
+							serverIp4 := net.IPv4(ip4[0], byte(i), byte(j), byte(k))
+							connectToServer(serverIp4, files)
+							if done { return }
+						}
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("Scanning 10.0.0.0 to 10.255.255.255 range...")
+	for _, ip := range ips {
+		ip4 := ip.To4()
+		if ip4 != nil && ip4[0] == 10 {
+			for i := 0; i <= 255; i++ {
+				for j := 0; j <= 255; j++ {
+					for k := 0; k <= 255; k++ {
+						if byte(i) != ip4[1] && byte(j) != ip4[2] && byte(k) != ip4[3] {
+							serverIp4 := net.IPv4(ip4[0], byte(i), byte(j), byte(k))
+							connectToServer(serverIp4, files)
+							if done { return }
+						}
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("Receiver not found, press a key to exit")
 }
 
 func sendFiles(files []string, ip string) {
